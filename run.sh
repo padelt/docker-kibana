76942ad6ab8d9ce9e0b1385193b298754c9ec3ae
Philipp Adelt <info@philipp.adelt.net>
2015-01-17 15:36:03 +0100

    fester lokaler Webserver-Port

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d -p 10000:10000 -p 80 -p 9200:9200 -v /docker-volumes/kibana/elasticsearch-logs:/elasticsearch/logs -v /docker-volumes/kibana/logstash:/logstash -v /docker-volumes/kibana/elasticsearch-data:/elasticsearch/data --name kibana kibana
+docker run -d -p 10000:10000 -p 9280:80 -p 9200:9200 -v /docker-volumes/kibana/elasticsearch-logs:/elasticsearch/logs -v /docker-volumes/kibana/logstash:/logstash -v /docker-volumes/kibana/elasticsearch-data:/elasticsearch/data --name kibana kibana
